fd3f8941b253454df1cffbbb85f091a1a270226d
Radina <49740981+radinag@users.noreply.github.com>
2019-04-29 13:17:36 -0400

    Initial commit

---
+++ inc/footer.php
@@ -0,0 +1,19 @@
+</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="http://twitter.com" target="_blank">Twitter</a></li>
+				<li><a href="https://www.facebook.com" target="_blank">Facebook</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?> Personal Media Library</p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
